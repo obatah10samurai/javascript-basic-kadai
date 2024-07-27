47abaa9b457fa0fadad25a9bd6561585b8205a72
obatah10samurai <obata.h10.samurai@gmail.com>
2024-07-28 00:57:12 +0900

    kadai_005　課題提出

---
+++ kadai_005/bmi.js
@@ -0,0 +1,12 @@
+// const weight = 68;
+// const height = 1.7;
+// // bmi：[Weight(kg)]÷[Height(m)×Height(m)]
+
+// console.log((weight) / ( height * height));
+
+
+
+let weight = 68;
+let height = 1.7;
+let bmi = weight / (height * height);
+console.log(bmi);
